390f9dea4ed28859b960512a8d0c7f828a0765a4
haveyouheardofthehighelves <32472487+haveyouheardofthehighelves@users.noreply.github.com>
2021-01-16 12:57:22 -0800

    UnityProjectExported

---
+++ Archer/Assets/ArrowMotion.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMotion : MonoBehaviour
+{   
+    Rigidbody2D rb;
+    bool OnHit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnHit = false;
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        rb.AddForce(Vector2.right*2000);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!OnHit)
+        {
+            float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag != "Player")
+        {
+            OnHit = true;
+        }
+    }
+}
---
+++ Archer/Assets/PlayerMovement.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{   [HeaderAttribute("Groundstuff")]
+    bool Grounded = false;
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void Update()
+    {
+        GroundMovement(2500);
+        if (!Grounded)
+        {
+            WallJump(.3f,100); 
+        }
+    }
+    private Transform FindChild(Transform a,string find)
+    {
+        foreach(Transform child in a)
+        {
+            if(child.name == find){
+                return child;
+            }
+        }
+        Debug.Log(find + " does not exist under " + a.name);
+        return null;
+    }
+    float oldwall = 0.1f;
+    float newwall = .2f;
+    private void WallJump(float slidespeed, float jumpforce)
+    {
+       
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        RaycastHit2D a = Physics2D.Raycast(FindChild(transform, "Body").position, Vector2.left,1.2f, ~LayerMask.GetMask("Player"));
+        RaycastHit2D b = Physics2D.Raycast(FindChild(transform, "Body").position, -Vector2.left, 1.2f, ~LayerMask.GetMask("Player"));
+        if (a)
+        {
+            newwall = a.collider.GetInstanceID();
+            if(oldwall != newwall)
+            {
+                rb.velocity = new Vector3(rb.velocity.x, Mathf.Clamp(rb.velocity.y, slidespeed, float.MaxValue), 0);
+                if (Input.GetKeyDown("space"))
+                {
+                   oldwall = newwall;
+                    rb.AddForce(Vector2.up * jumpforce,ForceMode2D.Impulse);
+                }
+            }
+
+        }
+        else if (b)
+        {
+            newwall = b.collider.GetInstanceID();
+            if (oldwall != newwall)
+            {
+                rb.velocity = new Vector3(rb.velocity.x, Mathf.Clamp(rb.velocity.y, slidespeed, float.MaxValue), 0);
+                if (Input.GetKeyDown("space"))
+                {
+                    oldwall = newwall;
+                    rb.AddForce(Vector2.up * jumpforce,ForceMode2D.Impulse);
+                }
+            }
+
+        }
+
+    }
+    private void GroundMovement(float movespeed)
+    {
+      Rigidbody2D rb = GetComponent<Rigidbody2D>();
+      float x =  Input.GetAxis("Horizontal")*Time.deltaTime*movespeed;
+      if(Physics2D.CircleCast(FindChild(transform, "Body").position - FindChild(transform, "Body").up,.4f,Vector2.down,1f, ~LayerMask.GetMask("Player")))
+        {
+            Grounded = true;
+            ResetWalls();
+            Jump(75);
+        }
+        else
+            Grounded = false;
+        Vector2 move = new Vector2(x, rb.velocity.y);
+        rb.velocity = move;
+    }   
+    void Jump(float jumpforce)
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (Input.GetKeyDown("space"))
+            rb.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
+    }
+    private void ResetWalls()
+    {    oldwall = 0.1f;
+         newwall = .2f;
+    }
+}
